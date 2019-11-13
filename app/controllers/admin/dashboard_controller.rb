596affde9ce6e0515e589c997d8e5ffebe3f5f42
tyler martin <t.mar92@gmail.com>
2019-11-13 12:08:50 -0500

    adds details about database to dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @category_count = Category.count
+    @product_count = Product.count
+    @total = 0
+    @all_products = Product.all.each do |item|
+      @total += item.quantity
+    end
   end
 end
